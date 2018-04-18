64466742b8103f40a5e14dffdc0724c347be3cf6
mikeheiser9 <35270182+mikeheiser9@users.noreply.github.com>
2018-04-18 15:42:11 -0700

    Update server.js

--- server.js
+++ server.js
@@ -27,9 +27,9 @@ var databaseUri = "mongodb://localhost/mongoHW"
 
 // Connect to MongoDB
 // mongoose.connect("mongodb://localhost/mongoHW");
-console.log(process.env.MONOGODB_URI)
-if(process.env.MONOGODB_URI) {
-  mongoose.connect(process.env.MONOGODB_URI);
+console.log(process.env.MONGODB_URI)
+if(process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
 } else {
   mongoose.connect(databaseUri);
 }
